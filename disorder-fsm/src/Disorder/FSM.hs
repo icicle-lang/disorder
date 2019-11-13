2bb14db6b4e33ced3f476db89e0b39163e2fe599
Huw Campbell <huw.campbell@gmail.com>
2019-11-13 16:14:04 +1100

    Remove most of the project

--- disorder-fsm/src/Disorder/FSM.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE NoImplicitPrelude #-}
-module Disorder.FSM (
-    module X
-  ) where
-
-import           Disorder.FSM.Catch as X
-import           Disorder.FSM.Core as X
-import           Disorder.FSM.Property as X
-import           Disorder.FSM.Runner as X
